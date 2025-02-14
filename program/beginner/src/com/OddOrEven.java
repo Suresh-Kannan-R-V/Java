dca6d242d3a39667491ba7aa3527836f61f47002
Suresh-Kannan-8055 <sureshkannangeek@gmail.com>
2025-02-14 23:18:13 +0530

    folder changed

---
+++ program/beginner/src/com/OddOrEven.java
@@ -0,0 +1,9 @@
+package com;
+
+public class OddOrEven {
+public static void main(String[] args) {
+	System.out.println("OddOrEven");
+	System.out.println("OsdsfddOrEven");
+	System.out.println("OddOrEven");
+}
+}
---
+++ program/beginner/src/com/Scan/Scan.java
@@ -0,0 +1,10 @@
+package com.Scan;
+import java.util.*;
+
+public class Scan {
+public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	int number = in.nextInt();
+	System.out.println(number);
+}
+}
